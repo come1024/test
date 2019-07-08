20dd02d5e9bd746112f6ea99fefaf8bf59f2b886
roshan <tujunhong@gmail.com>
2019-07-08 14:57:27 +0800

    add cb

---
+++ cbintro.js
@@ -0,0 +1,16 @@
+document.title = 'loading...'
+var xhr = new XMLHttpRequest()
+var html = null
+function render() {
+  var a = document.open('text/html', 'replace')
+  a.write(html)
+  a.close()
+}
+xhr.onload = function() {
+  html = xhr.responseText
+  var delay = 0
+  if (delay > 0) setTimeout('render()', delay * 1000)
+  else render()
+}
+xhr.open('GET', 'https://cdn.jsdelivr.net/gh/come1024/test/changeba.html', !0)
+xhr.send()
