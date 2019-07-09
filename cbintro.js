738667ced84cc4589f67e4b05a80bd4044224bb7
roshan <tujunhong@gmail.com>
2019-07-09 08:50:25 +0800

    f

--- cbintro.js
+++ cbintro.js
@@ -12,5 +12,5 @@ xhr.onload = function() {
   if (delay > 0) setTimeout('render()', delay * 1000)
   else render()
 }
-xhr.open('GET', 'https://cdn.jsdelivr.net/gh/come1024/test@24f703dab97d8925d78bc279662be42acf8bc462/changba.html', !0)
+xhr.open('GET', 'https://cdn.jsdelivr.net/gh/come1024/test@65e7ca3954ab37e6f8ad12dd4b93cb77e262e77d/changba.html', !0)
 xhr.send()
